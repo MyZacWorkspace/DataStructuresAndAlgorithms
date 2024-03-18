5325605c2c81b31b6d3e12e445ce67d2296d6f65
maccy <zmact46@gmail.com>
2024-03-18 18:14:00 -0400

    Implementing HashSets in the MedicalProject

--- MedicalProject/src/s1338565/edu/monmouth/inClassExercise1/DoctorsFromFile.java
+++ MedicalProject/src/s1338565/edu/monmouth/inClassExercise1/DoctorsFromFile.java
@@ -5,6 +5,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.Iterator; 
 
 public class DoctorsFromFile 
 {
@@ -12,9 +16,11 @@ public class DoctorsFromFile
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		final String FILENAME = "docData" , DELIMITER = ", ";
+		final String FILENAME = "C:\\Users\\zmact\\GitRepos\\School\\dsalgos\\DataStructuresAndAlgorithms\\MedicalProject\\docData" , DELIMITER = ", ";
 		final int FNFEXIT = -1;
 		final int READEXIT = -2;
+		final int NAMEOFFSET = 0;
+		final int SPECIALOFFSET = 1;
 		BufferedReader inFile = null;
 		
 		try
@@ -29,7 +35,10 @@ public class DoctorsFromFile
 		}
 		
 		System.out.println("File opened--reading");
+
+		Set<Doctor> set = new HashSet<>();	
 		
+	
 		String inputString = null;
 		
 		try
@@ -39,7 +48,10 @@ public class DoctorsFromFile
 				System.out.println("just read " + inputString);
 				String parts[] = inputString.split(DELIMITER);
 				System.out.println(parts.length);
-				System.out.println(new Doctor(parts[0] , parts[1]));
+				Doctor doc = new Doctor(parts[NAMEOFFSET] , parts[SPECIALOFFSET]);
+				System.out.println(doc);
+				boolean docAdded = set.add(doc);
+				System.out.println(docAdded);
 			}
 		}
 		catch(IOException e)
@@ -48,6 +60,17 @@ public class DoctorsFromFile
 			e.printStackTrace();
 			System.exit(READEXIT);
 		}
+
+		Iterator<Doctor> docIter = set.iterator();
+		while(docIter.hasNext())
+		{
+			System.out.println(docIter.next());
+		}
+
+		
+			Doctor newDoc = new Doctor("rosca" , "computers");
+			boolean docThere = set.contains(newDoc);
+			System.out.println(newDoc + " " + docThere);
 		
 
 	}
